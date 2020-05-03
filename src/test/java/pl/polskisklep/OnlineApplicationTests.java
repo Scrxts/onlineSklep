2a925863a935f17209b63869ea8c322124f966c6
scrxts <djtotyshs@gmail.com>
2020-05-03 22:38:52 +0200

    Initial commit

---
+++ src/test/java/pl/polskisklep/OnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polskisklep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
